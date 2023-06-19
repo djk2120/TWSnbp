5a92ca828046ef6c090977aebd7951afbe8ba910
Daniel Kennedy <djk2120@columbia.edu>
2023-06-19 16:32:50 -0600

    cesm2 postp, actually

--- postp/utils.py
+++ postp/utils.py
@@ -32,8 +32,14 @@ def get_sw(file):
         if 'lev' in dim:
             sdim=dim
     ns=len(ds.H2OSOI[sdim])
-    dz=xr.DataArray(1000*ds.DZSOI.sel(lat=30,lon=105,method='nearest')[:ns].values,
-                    dims=sdim,name='Soil thickness',attrs={'units':'m'})
+    if 'DZSOI' in ds.data_vars:
+        dz=xr.DataArray(1000*ds.DZSOI.sel(lat=30,lon=105,method='nearest')[:ns].values,
+                        dims=sdim,name='Soil thickness',attrs={'units':'m'})
+    elif 'CESM2' in file:
+        f='/glade/campaign/cgd/cesm/CESM2-LE/timeseries/lnd/proc/tseries/month_1/NBP/b.e21.BHISTcmip6.f09_g17.LE2-1001.001.clm2.h0.NBP.192001-192912.nc'
+        tmp=xr.open_dataset(f)
+        dz=xr.DataArray(1000*tmp.DZSOI.sel(lat=30,lon=105,method='nearest')[:ns].values,
+                        dims=sdim,name='Soil thickness',attrs={'units':'m'})
     sw=(dz*ds.H2OSOI).sum(dim=sdim).compute()
     
     return sw
